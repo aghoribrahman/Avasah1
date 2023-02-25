1d6804ba4325245b17abe33c8ee6ade17a35ab73
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2023-02-25 15:29:51 +0530

    back zero

--- venv/avasah1/build_files.sh
+++ venv/avasah1/build_files.sh
@@ -2,7 +2,5 @@
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD ENDS"
 
-echo " Make Migrations"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
